130c3f67f0d9e08e33c01c889ca93083645ef5e7
Anthony <anthonygibson24@gmail.com>
2022-05-16 22:50:37 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import BoxGen from './components/BoxGen';
 
 function App() {
+
+  const squareStyle = {
+    width: '20px',
+    height: '20px',
+    backgroundColor: 'red'
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BoxGen />
+      {/* <div style={squareStyle}></div> */}
     </div>
   );
 }
---
+++ src/components/BoxGen.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useState } from 'react';
+
+const BoxGen = (props) => {
+    const [userColor, setUserColor] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e.target.value);
+        console.log('userColor:' + userColor);
+        const submitted = true;
+        
+    }
+
+    const squareStyle = {
+        width: '20px',
+        height: '20px',
+        backgroundColor: 'red'
+    }
+    const createDiv = <div style={squareStyle}></div>;
+
+
+    return (
+        <form onSubmit={ handleSubmit }>
+            <label>Color</label>
+            <input
+            onChange={ (e) => setUserColor(e.target.value)}
+            value={ userColor }
+            ></input>
+            <button type="submit">Add</button>
+            <div>{createDiv}</div>
+        </form>
+
+
+
+    )
+}
+
+export default BoxGen;
